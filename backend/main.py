5b1f51b628007e6bce1c59ad4496379687d459f7
Saurav Sarkar <saurav0988@gmail.com>
2023-12-03 23:28:41 -0700

    Added CORS

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import artwork_router
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(artwork_router.router)
