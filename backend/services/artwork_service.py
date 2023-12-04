7527a222b9b406f8f8d076a1006bd5c2b35f608d
Saurav Sarkar <saurav0988@gmail.com>
2023-12-03 20:43:52 -0700

    Fixed SPARQL

--- backend/services/artwork_service.py
+++ backend/services/artwork_service.py
@@ -3,20 +3,19 @@ from typing import List, Optional
 from model.models import ArtworkSearchResult
 from constants import SPARQL_SERVER_URL
 
-def build_sparql_query(title: Optional[str], artist_name: Optional[str], medium: Optional[str], museum_name: Optional[str]) -> str:
+def build_sparql_query(title: Optional[str], artist_name: Optional[str], classification_name: Optional[str]) -> str:
     """
     Construct the SPARQL query based on the provided search parameters.
     """
     query_parts = []
 
     if title:
-        query_parts.append(f'FILTER regex(?title, "{title}")')
+        query_parts.append(f'FILTER regex(?artifactTitle, "{title}")')
     if artist_name:
         query_parts.append(f'FILTER regex(?artistName, "{artist_name}")')
-    if medium:
-        query_parts.append(f'FILTER regex(?medium, "{medium}")')
-    if museum_name:
-        query_parts.append(f'FILTER regex(?museumName, "{museum_name}")')
+    if classification_name:
+        query_parts.append(f'FILTER regex(?classificationName, "{classification_name}")')
+
 
     filter_query = " . ".join(query_parts) if query_parts else ""
 
@@ -25,32 +24,60 @@ def build_sparql_query(title: Optional[str], artist_name: Optional[str], medium:
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX umt: <http://www.semanticweb.org/artifact#>
-    SELECT ?artistName ?title ?artworkID ?productionYear ?dimensions ?description ?classification ?imageUrl ?width ?height ?nationality ?site ?room ?museumName ?lifePeriod ?artistDesc ?artistImageUrl ?medium 
+    PREFIX umt: <http://www.semanticweb.org/nvsp/ontologies/2023/11/semantic-museum.owl#>
+    SELECT 
+        ?artifactId
+        ?artifactImage
+        ?artifactTitle
+        ?artifactWidth
+        ?artifactHeight
+        ?depth
+        ?diameter
+        ?department
+        ?location
+        ?medium
+        ?provenance
+        ?artistId
+        ?artistImage
+        ?artistName
+        ?birthDate
+        ?birthPlace
+        ?deathDate
+        ?deathPlace
+        ?nationality
+        ?classificationName
     WHERE {{
-        ?art rdf:type umt:Artifact;
-        umt:hasTitle ?title;
-        umt:isCreatedBy ?art2;
+        ?artifact rdf:type umt:Artifact;
+        umt:hasArtifactDepth ?depth;
+        umt:hasArtifactDiameter ?diameter;
+        umt:hasArtifactHeight ?height;
+        umt:hasArtifactID ?artifactId;
+        umt:hasArtifactImage ?artifactImage;
+        umt:hasArtifactTitle ?artifactTitle;
+        umt:hasArtifactWidth ?artifactWidth;
+        umt:hasDepartment ?department;
+        umt:hasLocation ?location;
         umt:hasMedium ?medium;
-        umt:hasDate ?productionYear;
-        umt:hasArtifactID ?artworkID;
-        umt:hasDimensions ?dimensions;
-        umt:hasDescription ?description;
-        umt:hasClassification ?classification;
-        umt:hasImage ?imageUrl;
-        umt:hasHeight ?height;
-        umt:hasWidth ?width;
-        umt:hasSite ?site;
-        umt:hasRoom ?room;
-        umt:hasMuseumName ?museumName.
-    
-    
-        ?art2 rdf:type umt:Artist;
+        umt:hasProvenance ?provenance;
+        umt:hasArtist ?artist;
+        umt:hasClassification ?classification.
+        # umt:hasRole ?role.
+
+
+        ?artist rdf:type umt:Artist;
+        umt:hasArtistID ?artistId;
+        umt:hasArtistImage ?artistImage;
         umt:hasArtistName ?artistName;
-        umt:hasNationality ?nationality;
-        umt:hasLifeperiod ?lifePeriod;
-        umt:hasArtistDescr ?artistDesc;
-        umt:hasArtistImage ?artistImageUrl.
+        umt:hasBirthDate ?birthDate;
+        umt:hasBirthPlace ?birthPlace;
+        umt:hasDeathDate ?deathDate;
+        umt:hasDeathPlace ?deathPlace;
+        umt:hasNationality ?nationality.
+
+
+        ?classification rdf:type umt:Classification;
+        umt:hasClassificationName ?classificationName.
+
         {filter_query}
     }}
     LIMIT 30000
@@ -73,31 +100,36 @@ def map_results_to_model(results):
     for r in results['results']['bindings']:
         try:
             artwork = ArtworkSearchResult(
-                artistName=r['artistName']['value'],
-                artworkTitle=r['title']['value'],
-                artworkWidth=r.get('width', {}).get('value'),
-                artworkImage=r['imageUrl']['value'],
-                artworkId=r['artworkID']['value'],
-                productionYear=r['productionYear']['value'],
-                nationality=r['nationality']['value'],
-                site=r["site"]['value'],
-                room=r["room"]['value'],
-                museumName=r['museumName']['value'],
-                lifePeriod=r["lifePeriod"]["value"],
-                artistDesc=r['artistDesc']['value'],
-                artistImage=r["artistImageUrl"]['value'],
-                medium=r['medium']['value'],
-                # ... map other fields as needed ...
+                artifactId=r.get('artifactId', {}).get('value'),
+                artifactImage=r.get('artifactImage', {}).get('value'),
+                artifactTitle=r.get('artifactTitle', {}).get('value'),
+                artifactWidth=r.get('artifactWidth', {}).get('value'),
+                artifactHeight=r.get('artifactHeight', {}).get('value'),
+                depth=r.get('depth', {}).get('value'),
+                diameter=r.get('diameter', {}).get('value'),
+                department=r.get('department', {}).get('value'),
+                location=r.get('location', {}).get('value'),
+                medium=r.get('medium', {}).get('value'),
+                provenance=r.get('provenance', {}).get('value'),
+                artistId=r.get('artistId', {}).get('value'),
+                artistImage=r.get('artistImage', {}).get('value'),
+                artistName=r.get('artistName', {}).get('value'),
+                birthDate=r.get('birthDate', {}).get('value'),
+                birthPlace=r.get('birthPlace', {}).get('value'),
+                deathDate=r.get('deathDate', {}).get('value'),
+                deathPlace=r.get('deathPlace', {}).get('value'),
+                nationality=r.get('nationality', {}).get('value'),
+                classificationName=r.get('classificationName', {}).get('value')
             )
             artworks.append(artwork)
         except KeyError:
             pass
     return artworks
 
-def search_artwork(title: Optional[str] = None, artist_name: Optional[str] = None, medium: Optional[str] = None, museum_name: Optional[str] = None) -> List[ArtworkSearchResult]:
+def search_artwork(title: Optional[str] = None, artist_name: Optional[str] = None, classification_name: Optional[str] = None) -> List[ArtworkSearchResult]:
     """
     Perform artwork search based on given parameters.
     """
-    query = build_sparql_query(title, artist_name, medium, museum_name)
+    query = build_sparql_query(title, artist_name, classification_name)
     results = execute_sparql_query(query)
     return map_results_to_model(results)
